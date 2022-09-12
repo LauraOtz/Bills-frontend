2ab03c304487256ad908b1b2d2851c967cea1f42
Laura Ortiz <bjtamyl@hotmail.com>
2022-09-12 16:52:31 -0300

    responsive login

--- src/pages/login/TextHome.jsx
+++ src/pages/login/TextHome.jsx
@@ -1,17 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import logoBills1 from "../../assets/logoBills1.png"
+import logoBills1 from "../../assets/logoBills1.png";
 
 import "../home/home.css";
 import "./login.css";
 function TextHome() {
   return (
-    <Container fluid className="TextHomeContainer">
-      <Row>
+    <Container>
+      <Row fluid className="TextHomeContainer">
         <img src={logoBills1} alt="" className="img-c d-flex" />
         <Col xs={8} md={11} className="d-flex justify-content-start text-light">
-          <h1 className= "h1T text-light">
+          <h1 className="h1T text-light">
             Generá tus presupuestos con un "Click"
           </h1>
         </Col>
