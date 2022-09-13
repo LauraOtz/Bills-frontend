3395cbac55d06add3ea2740546905e376c361f58
Laura Ortiz <bjtamyl@hotmail.com>
2022-09-13 14:11:27 -0300

    pull lau

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,10 @@
-
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import LayoutApp from '../../components/Layout'
-import { Row, Col } from 'antd';
-import Product from '../../components/Product';
-import { useDispatch } from 'react-redux';
-import "../home/home.css"
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import LayoutApp from "../../components/Layout";
+import { Row, Col } from "antd";
+import Product from "../../components/Product";
+import { useDispatch } from "react-redux";
+import "../home.css";
 const Home = () => {
   const dispatch = useDispatch();
 
