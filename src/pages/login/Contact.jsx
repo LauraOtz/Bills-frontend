fdd4a1daf712387ab073caf61d6a850d3ec4734a
rdanielpolich <rdanielpolich@gmail.com>
2022-09-20 19:28:11 -0300

    arreglado responsive para seccion de productos

--- src/pages/login/Contact.jsx
+++ src/pages/login/Contact.jsx
@@ -1,14 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { Button, Form, Input, Row, Col } from "antd";
-<<<<<<< HEAD
 import "../login/Contact.css";
-=======
-
-
-import "../login/Contact.css";
-
->>>>>>> 7e781aa3113192e4e5c18a3d7b47834d0ff7ad3b
 
 const { TextArea } = Input;
 
@@ -32,157 +25,153 @@ const Contact = () => {
   };
 
   return (
- 
     <>
       <hr className="hr" />
 
-      <Row >
-        <div  className="ContainerContactanos" >
+      <Row>
+        <div className="ContainerContactanos">
+          <Col offset={0} className="contC">
+            <div>
+              <h1 span={12} className="H1Contactanos">
+                Contáctanos
+              </h1>
 
-       
-        <Col offset={0} className="contC">
-          <div>
-            <h1 span={12} className="H1Contactanos">
-              Contáctanos
-            </h1>
-
-            <h3 className="H3Contactanos">
-              ¿Querés hacer tus tareas más faciles, responder a tus clientes al
-              instante y mejorar la dinamica de tu empresa? dejanos tus datos y
-              responderemos a la brevedad.
-            </h3>
-            <h3 className="H3Contactanos textC">
-              Personalizamos tu cuenta a tus necesitades!
-            </h3>
-          </div>
-        </Col>
-        <Col offset={0} className="contC">
-          <Form
-            className="FormContactanos"
-            ref={form}
-            onFinish={sendEmail}
-            name="basic"
-            id="FormContactanos"
-            labelCol={{
-              span: 8,
-            }}
-            wrapperCol={{
-              span: 16,
-            }}
-            initialValues={{
-              remember: true,
-            }}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Nombre"
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: "Por favor introduzca su nombre",
-                },
-
-                {
-                  type: "regexp",
-                  pattern: new RegExp("^[a-zA-Z]*$"),
-                  message: "Por favor ingrese un nombre válido",
-                },
-
-                {
-                  min: 2,
-                  max: 20,
-                  message: "En nombre debe contener entre 2 y 20 Caracteres",
-                },
-                {
-                  validator: (_, value) =>
-                    value.match(/^[a-zA-Z]+$/)
-                      ? Promise.resolve()
-                      : Promise.reject(
-                          "Por favor ingrese sólo letras en el nombre."
-                        ),
-                },
-              ]}
-              hasFeedback
-            >
-              <Input placeholder="Ingrese su nombre." />
-            </Form.Item>
-            <Form.Item
-              label="Correo electrónico"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Por favor introduzca su correo electrónico.",
-                },
-                {
-                  type: "email",
-                  message: "Por favor ingrese un correo electrónico válido",
-                },
-                {
-                  whitespace: true,
-                },
-              ]}
-              hasFeedback
-            >
-              <Input placeholder="Ingrese su correo electrónico" />
-            </Form.Item>
-
-            <Form.Item
-              name="message"
-              label="Mensaje"
-              rules={[
-                {
-                  required: true,
-                  message: "Por favor ingrese un mensaje",
-                },
-                {
-                  min: 4,
-                  max: 400,
-                  message:
-                    "Ingrese un mensaje válido, entre 4 y 400 caracteres",
-                },
-
-                {
-                  whitespace: true,
-                },
-              ]}
-              hasFeedback
-            >
-              <TextArea
-                name="textarea"
-                cols={100}
-                rows={4}
-                showCount
-                maxLength={400}
-                hasFeedback
-                placeholder="Ingrese su consulta"
-              />
-            </Form.Item>
-
-            <Form.Item
+              <h3 className="H3Contactanos">
+                ¿Querés hacer tus tareas más faciles, responder a tus clientes
+                al instante y mejorar la dinamica de tu empresa? dejanos tus
+                datos y responderemos a la brevedad.
+              </h3>
+              <h3 className="H3Contactanos textC">
+                Personalizamos tu cuenta a tus necesitades!
+              </h3>
+            </div>
+          </Col>
+          <Col offset={0} className="contC">
+            <Form
+              className="FormContactanos"
+              ref={form}
+              onFinish={sendEmail}
+              name="basic"
+              id="FormContactanos"
+              labelCol={{
+                span: 8,
+              }}
               wrapperCol={{
-                offset: 20,
                 span: 16,
               }}
+              initialValues={{
+                remember: true,
+              }}
+              autoComplete="off"
             >
-              <Button
-                type="primary"
-                htmlType="submit"
-                value="Send"
-                class="ant-btn ant-btn-default add-new"
-                className="add-new "
-                style={{ fontWeight: "bold" }}
+              <Form.Item
+                label="Nombre"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Por favor introduzca su nombre",
+                  },
+
+                  {
+                    type: "regexp",
+                    pattern: new RegExp("^[a-zA-Z]*$"),
+                    message: "Por favor ingrese un nombre válido",
+                  },
+
+                  {
+                    min: 2,
+                    max: 20,
+                    message: "En nombre debe contener entre 2 y 20 Caracteres",
+                  },
+                  {
+                    validator: (_, value) =>
+                      value.match(/^[a-zA-Z]+$/)
+                        ? Promise.resolve()
+                        : Promise.reject(
+                            "Por favor ingrese sólo letras en el nombre."
+                          ),
+                  },
+                ]}
+                hasFeedback
               >
-                Enviar
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
+                <Input placeholder="Ingrese su nombre." />
+              </Form.Item>
+              <Form.Item
+                label="Correo electrónico"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Por favor introduzca su correo electrónico.",
+                  },
+                  {
+                    type: "email",
+                    message: "Por favor ingrese un correo electrónico válido",
+                  },
+                  {
+                    whitespace: true,
+                  },
+                ]}
+                hasFeedback
+              >
+                <Input placeholder="Ingrese su correo electrónico" />
+              </Form.Item>
+
+              <Form.Item
+                name="message"
+                label="Mensaje"
+                rules={[
+                  {
+                    required: true,
+                    message: "Por favor ingrese un mensaje",
+                  },
+                  {
+                    min: 4,
+                    max: 400,
+                    message:
+                      "Ingrese un mensaje válido, entre 4 y 400 caracteres",
+                  },
+
+                  {
+                    whitespace: true,
+                  },
+                ]}
+                hasFeedback
+              >
+                <TextArea
+                  name="textarea"
+                  cols={100}
+                  rows={4}
+                  showCount
+                  maxLength={400}
+                  hasFeedback
+                  placeholder="Ingrese su consulta"
+                />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 20,
+                  span: 16,
+                }}
+              >
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  value="Send"
+                  class="ant-btn ant-btn-default add-new"
+                  className="add-new "
+                  style={{ fontWeight: "bold" }}
+                >
+                  Enviar
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
         </div>
       </Row>
     </>
-  
   );
 };
 export default Contact;
