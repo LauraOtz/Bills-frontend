7e781aa3113192e4e5c18a3d7b47834d0ff7ad3b
DamiAbi <damithapam@gmail.com>
2022-09-20 17:55:06 -0300

    s

--- src/pages/login/Contact.jsx
+++ src/pages/login/Contact.jsx
@@ -2,11 +2,9 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { Button, Form, Input, Row, Col } from "antd";
 
-<<<<<<< HEAD
+
 import "../login/Contact.css";
-=======
-import "../login/Contactanos.css";
->>>>>>> c58eda7ddc6b43c1c7822f41c0cbf41a4867a479
+
 
 const { TextArea } = Input;
 
